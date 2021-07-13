c8c114e2290984f2783f8fd46c1d93ef111fb716
ottovisocks <otto.visocks@gmail.com>
2021-07-13 11:36:57 +0300

    Updated complited task

--- csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Casting
 {
@@ -17,25 +13,25 @@ namespace Casting
 
         static void first()
         {
-            int a = 1;
+            String a = "1";
             int b = 2;
             int c = 3;
-            int d = 4;
-            int e = 5;
-            int sum = a + b + c + d + e;
+            double d = 4;
+            float e = 5;
 
+            int sum = Convert.ToInt32(a) + b + c + (int)d + (int)e;
             Console.WriteLine(sum);
         }
 
         static void second()
         {
-            int a = 1;
+            String a = "1";
             int b = 2;
             int c = 3;
             double d = 4.2;
             float e = 5.3f;
-            float sum = (float)a + (float)b + (float)c + (float)d + e;
 
+            float sum = Convert.ToInt32(a) + b + c + (float)d + e;
             Console.WriteLine(sum);
         }
     }
