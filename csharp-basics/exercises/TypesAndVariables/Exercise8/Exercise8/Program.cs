06c2c0e8a9f630d1023e89f1ffad41b8e5ab015b
ottovisocks <otto.visocks@gmail.com>
2021-07-13 14:41:38 +0300

    Task comleted

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise8/Exercise8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes = 30;
+            int minutesInHour = 60;
+            int hoursInDay = 24;
+            int daysInYear = 365;
+            double minutesToDays = (double)minutes / minutesInHour / hoursInDay;
+            decimal minutesToYears = (decimal)minutesToDays / daysInYear;
+
+            Console.WriteLine($"{minutes} are {minutesToDays} days and {minutesToYears} of year.");
+            Console.ReadKey();
+        }
+    }
+}
