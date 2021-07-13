10f4ce41da48896b284592986878de6b7cd37175
ottovisocks <otto.visocks@gmail.com>
2021-07-13 12:19:21 +0300

    Task completed

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Otto";
+            ushort year = 1992;
+
+            Console.WriteLine($"My name is {name} and I was born in {year}.");
+            Console.ReadKey();
+        }
+    }
+}
