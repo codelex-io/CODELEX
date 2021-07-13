01de217d04df592c42f62b64023657239b69da46
ottovisocks <otto.visocks@gmail.com>
2021-07-13 11:19:00 +0300

    Task completed

--- csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Casting/Program.cs
@@ -17,28 +17,26 @@ namespace Casting
 
         static void first()
         {
-            String a = "1";
+            int a = 1;
             int b = 2;
             int c = 3;
-            double d = 4;
-            float e = 5;
+            int d = 4;
+            int e = 5;
+            int sum = a + b + c + d + e;
 
-            //fixme - should be 15 :|
-            //int sum = a + b + c + d + e;
-            //Console.WriteLine(sum);
+            Console.WriteLine(sum);
         }
 
         static void second()
         {
-            String a = "1";
+            int a = 1;
             int b = 2;
             int c = 3;
             double d = 4.2;
             float e = 5.3f;
+            float sum = (float)a + (float)b + (float)c + (float)d + e;
 
-            //fixme - should be 15.5 :| 
-            //float sum = a + b + c + d + e;
-            //Console.WriteLine(sum);
+            Console.WriteLine(sum);
         }
     }
 }
