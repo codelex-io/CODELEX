f296149ea99872c6a7a2cd720bf95530f96a4c04
ottovisocks <otto.visocks@gmail.com>
2021-07-13 15:36:51 +0300

    Task completed

--- csharp-basics/exercises/TypesAndVariables/SadFor/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/SadFor/Program.cs
@@ -11,16 +11,13 @@ namespace SadFor
 
         private static void Test1()
         {
-            //fixme so I would compile
-            /*
-            for (i = 0; i < 10; i++)
+            for (int i = 0; i < 10; i++)
             {
                 if (Math.Sqrt(i) > 2.5)
                 {
                     Console.WriteLine(i);
                 }
             }
-            */
         }
     }
 }
