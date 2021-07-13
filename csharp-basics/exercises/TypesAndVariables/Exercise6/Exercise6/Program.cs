774911017cd0c0d5c634bd942bcbe703847396e4
ottovisocks <otto.visocks@gmail.com>
2021-07-13 13:51:49 +0300

    Task comleted

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Enter as many digits as you want (only digits): ");
+                string input = Console.ReadLine();
+                int sum = input.ToCharArray().Select(item => Convert.ToInt32(item.ToString())).ToArray().Sum();
+
+                Console.WriteLine($"Sum of your entered numbers ir {sum}");
+                Console.WriteLine("------------------------------------------------------------------------");
+            }
+        }
+    }
+}
