b1ddf13d1c4dcbe8078f1205543b5927e14e20e4
ottovisocks <otto.visocks@gmail.com>
2021-07-13 11:07:58 +0300

    Task completed

--- csharp-basics/exercises/TypesAndVariables/MoreVariablesAndPrinting/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/MoreVariablesAndPrinting/Program.cs
@@ -6,26 +6,26 @@ namespace MoreVariablesAndPrinting
     {
         static void Main(string[] args)
         {
-            String myName, myEyes, myTeeth, myHair;
-            int myAge, myHeight, myWeight;
 
-            myName = "Zed A. Shaw";
-            myAge = 35;
-            myHeight = 74;  // inches
-            myWeight = 180; // lbs
-            myEyes = "Blue";
-            myTeeth = "White";
-            myHair = "Brown";
+            string name = "Zed A. Shaw";
+            string eyes = "Blue";
+            string teeth = "White";
+            string hair = "Brown";
+            int age = 35;
+            int heightInInches = 74;
+            int weightInLbs = 180;
+            double heightInCentimeters = heightInInches * 2.54;
+            double weightInKilos = 180 * 0.453592;
+            double ageHeightWeightSum = age + heightInCentimeters + weightInKilos;
 
-            Console.WriteLine("Let's talk about " + myName + ".");
-            Console.WriteLine("He's " + myHeight + " inches tall.");
-            Console.WriteLine("He's " + myWeight + " pounds heavy.");
-            Console.WriteLine("Actually, that's not too heavy.");
-            Console.WriteLine("He's got " + myEyes + " eyes and " + myHair + " hair.");
-            Console.WriteLine("His teeth are usually " + myTeeth + " depending on the coffee.");
+            Console.WriteLine($"Let's talk about {name}.");
+            Console.WriteLine($"He's {heightInCentimeters.ToString("0.00")} centimeters tall.");
+            Console.WriteLine($"He's {weightInKilos.ToString("0.00")} kilos heavy.");
+            Console.WriteLine($"Actually, that's not too heavy.");
+            Console.WriteLine($"He's got {eyes} eyes and {hair} hair.");
+            Console.WriteLine($"His teeth are usually {teeth} depending on the coffee.");
 
-            Console.WriteLine("If I add " + myAge + ", " + myHeight + ", and " + myWeight
-                               + " I get " + (myAge + myHeight + myWeight) + ".");
+            Console.WriteLine($"If I add {age}, {heightInCentimeters.ToString("0.00")}, and {weightInKilos.ToString("0.00")} I get {ageHeightWeightSum.ToString("0.00")}.");
 
             Console.ReadKey();
         }
