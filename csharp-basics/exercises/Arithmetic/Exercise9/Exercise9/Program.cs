8076dc2491132b811671e482f61aa9c3d5df693b
ottovisocks <otto.visocks@gmail.com>
2021-07-14 16:07:02 +0300

    Exercise9

---
+++ csharp-basics/exercises/Arithmetic/Exercise9/Exercise9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your weight, kg: ");
+            double inputWeight = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter your height, cm: ");
+            double inputHeight = double.Parse(Console.ReadLine());
+
+            double weightToPounds = inputWeight * 2.205;
+            double heightToInches = inputHeight * 0.394;
+            double bmi = weightToPounds * 703 / Math.Pow(heightToInches, 2);
+
+            if(bmi > 18.5 && bmi < 25)
+            {
+                Console.WriteLine("Your weight is optimal");
+            } else if (bmi < 18.5)
+            {
+                Console.WriteLine("Your are underweight");
+            } else
+            {
+                Console.WriteLine("Your are overweight");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
