fb68a56e31ef93a3ff2a5d345035d870e1c53d02
ottovisocks <otto.visocks@gmail.com>
2021-07-14 15:53:12 +0300

    Exercise2

---
+++ csharp-basics/exercises/Arithmetic/Exercise2/Exercise2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Program
+    {
+        static void CheckOddEven(int a)
+        {
+            if(a % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            } else
+            {
+                Console.WriteLine("Odd Number");
+            }
+
+            Console.WriteLine("Bye");
+        }
+
+        static void Main(string[] args)
+        {
+            CheckOddEven(19);
+            Console.ReadKey();
+        }
+    }
+}
