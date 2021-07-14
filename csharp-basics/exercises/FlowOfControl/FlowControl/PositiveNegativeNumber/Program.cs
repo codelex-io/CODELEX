ce52226bfa7e988616ec274eb7b103353474c53b
ottovisocks <otto.visocks@gmail.com>
2021-07-14 17:09:55 +0300

    Exercise2

--- csharp-basics/exercises/FlowOfControl/FlowControl/PositiveNegativeNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/PositiveNegativeNumber/Program.cs
@@ -6,15 +6,14 @@ namespace PositiveNegativeNumber
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the number.");
-            var input = Console.ReadKey();
-            /*
-            fixme
-            if (?)
+            Console.Write("Enter the number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            if(input > 0)
             {
                 Console.WriteLine("Number is positive");
             } 
-            else if (?) 
+            else if(input < 0) 
             {
                 Console.WriteLine("Number is negative");
             } 
@@ -22,7 +21,7 @@ namespace PositiveNegativeNumber
             {
                 Console.WriteLine("Number is zero");
             }
-            */
+            Console.ReadKey();
         }
     }
 }
