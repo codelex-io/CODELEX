f15dfc69a11d7170746160d40162ba9748235076
ottovisocks <otto.visocks@gmail.com>
2021-07-14 15:55:37 +0300

    Exercise3

---
+++ csharp-basics/exercises/Arithmetic/SumAverageRunningInt/SumAverageRunningInt/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SumAverageRunningInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int lowerBound = 1;
+            int upperBound = 100;
+            
+            for (int i = lowerBound; i <= upperBound; ++i)
+            {
+                sum += i;
+            }
+
+            decimal average = (decimal)sum / (decimal)upperBound;
+
+            Console.WriteLine($"The sum of {lowerBound} to {upperBound} is {sum}");
+            Console.WriteLine($"The average is {average}");
+            Console.ReadKey();
+        }
+    }
+}
