c9ad3fff9c660454c07f078ea6e21d0c9d4a6701
ottovisocks <otto.visocks@gmail.com>
2021-07-14 15:56:57 +0300

    Exercise4

---
+++ csharp-basics/exercises/Arithmetic/Exercise4/Exercise4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Product1ToN()
+        {
+            int number = 10;
+            int factorial = 1;
+
+            for(int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"Factorial of {number} is: {factorial}");
+        }
+
+        static void Main(string[] args)
+        {
+            Product1ToN();
+            Console.ReadKey();
+        }
+    }
+}
