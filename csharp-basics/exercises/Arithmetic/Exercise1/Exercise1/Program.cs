6bd9bea3661421cb932b029ac05d0ad3e96a4161
ottovisocks <otto.visocks@gmail.com>
2021-07-14 15:51:39 +0300

    Exercise1

---
+++ csharp-basics/exercises/Arithmetic/Exercise1/Exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        static bool CheckNumbers(int a, int b)
+        {
+            return a == 15 || b == 15 || a + b == 15 || a - b == 15 || b - a == 15 ? true : false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckNumbers(30, 15));
+            Console.ReadKey();
+        }
+    }
+}
